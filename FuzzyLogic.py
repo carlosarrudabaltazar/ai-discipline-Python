538a15a924a897c366e771eaf5bca50784c12e8e
Carlos Arruda <carlosarrudabaltazar@hotmail.com>
2024-03-26 10:03:26 -0300

    implementing meaning function

--- FuzzyLogic.py
+++ FuzzyLogic.py
@@ -3,18 +3,29 @@ import math;
 import statistics as stat;
 import matplotlib.pyplot as plt;
 
-class MeaningFunction (object):
+class MeaningCurve (object):
     def __init__(self,
                  realScale:list,
                  a:float,
                  b:float,
                  c:float,
-                 d:float = None):
+                 d:float = None,
+                 function:str = "gauss"):
         self.realScale = realScale;
         self.a = a;
         self.b = b;
         self.c = c;
         self.d = d;
+        self.selectedFunction = function;
+    
+    def getMeaningCurve(self) -> list:
+        match self.selectedFunction:
+            case "triangle":
+                return self.getTriangle();
+            case "gauss":
+                return self.getGauss();
+            case "trapezium":
+                return self.getTrapezium();
 
     def getGauss(self) -> list:
         pertinence = [];
@@ -60,12 +71,65 @@ class MeaningFunction (object):
         
         return pertinence;
 
+class MeaningFunction (object):
+    def __init__(self,
+                 realScale:list,
+                 fuzzyScale:list):
+        self.realScale = realScale;
+        self.fuzzyScale = fuzzyScale;
+
+    def getMeaningFunction(self,
+                           function:str="gauss"):
+        meaningFunction = {};
+        step = len(self.realScale) // (len(self.fuzzyScale) - 1);
+        lastMax = 0;
+        a = 0;
+        b = 0;
+        c = 0;
+        d = 0;
+
+        for i in range(0, len(self.fuzzyScale),1):
+            if (i == 0): 
+                a = lastMax;
+                b = lastMax;
+                c = lastMax + step;
+                meaningCurve = MeaningCurve(realScale=self.realScale,
+                                            a=a,
+                                            b=b,
+                                            c=c,
+                                            function=function);
+                meaningFunction[self.fuzzyScale[i]] = meaningCurve.getMeaningCurve();
+                lastMax = b;
+            elif (i == (len(self.fuzzyScale) - 1)):
+                a = ultimoMaximo;
+                b = self.realScale[len(self.realScale) - 1];
+                c = self.realScale[len(self.realScale) - 1];
+                meaningCurve = MeaningCurve(realScale=self.realScale,
+                                            a=a,
+                                            b=b,
+                                            c=c,
+                                            function=function);
+                meaningFunction[self.fuzzyScale[i]] = meaningCurve.getMeaningCurve();
+                ultimoMaximo = b;
+            else:
+                a = lastMax;
+                b = lastMax + step;
+                c = b + step;
+                meaningCurve = MeaningCurve(realScale=self.realScale,
+                                            a=a,
+                                            b=b,
+                                            c=c,
+                                            function=function);
+                meaningFunction[self.fuzzyScale[i]] = meaningCurve.getMeaningCurve();
+                ultimoMaximo = b;
+
+
 def main():
     x = [0,1,2,3,4,5,6,7,8,9,10];
-    pertinence = PertinenceFunction(realScale = x,
-                                    a = 5,
-                                    b = 10,
-                                    c = 10);
+    pertinence = MeaningFunction(realScale = x,
+                                 a = 5,
+                                 b = 10,
+                                 c = 10);
     print(pertinence.getGauss());
     plt.plot(pertinence.getGauss())
     plt.show()
